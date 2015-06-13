319c827737310912a868335afdb4647a83f06099
Tamer Shlash <mr.tamershlash@gmail.com>
2015-06-14 05:12:12 +0700

    added medication business logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,42 +7,26 @@ angular.module('starter.controllers', [])
 .controller('BasicCtrl', function($scope, $localstorage) {
   $scope.user = $localstorage.getObject('activeUser');
 })
-.controller('MedicationCtrl', function($scope) {
+.controller('MedicationCtrl', function($scope, $localstorage, Drugs) {
+  $scope.activeMedics = 'medications_' + $localstorage.get('activeId');
+  $scope.medications = $localstorage.getObject($scope.activeMedics) || [];
   $scope.showAddForm = false;
-  $scope.medicineName = '';
   $scope.medicine = '';
-  $scope.medicineList = [
-    'Acebutolol', 
-    'Acnocin',
-    'Adrenaline', 
-    'Baclofen',
-    'Buscopan', 
-    'Calcitriol',
-    'Calcium Acetate',
-    'Epivir', 
-    'Gabapentin', 
-    'Glipizide', 
-    'Imodium',
-    'Lamivudine',
-    'Lactase Fast Act',
-    'Lasix',
-    'Lipitor',
-    'Madopar',
-    'Meiact',
-    'Metformin',
-    'Nexium',  
-    'Penicillin', 
-    'Rapifen', 
-    'Singulair', 
-    'Tamiflu', 
-    'Warfarin', 
-    'Xyzal', 
-    'Zyloric'
-  ];
-  $scope.selectMedicine = function (med) {
+  $scope.medicineList = Drugs.names();
+  $scope.selectMedicine = function(med) {
     $scope.medicine = med;
   };
+  $scope.addMedicine = function(dosage, frequency) {
+    $scope.medications.push({
+      name: $scope.medicine,
+      dosage: dosage,
+      frequency: frequency
+    });
+    $localstorage.setObject($scope.activeMedics, $scope.medications);
+    $scope.showAddForm = false;
+  };
 })
+
 .controller('ImmunizationCtrl', function($scope) {})
 .controller('ReportsCtrl', function($scope, Camera) {
   $scope.getPhoto = function() {
@@ -61,14 +45,6 @@ angular.module('starter.controllers', [])
 .controller('MedicalHistoryCtrl', function($scope) {})
 
 .controller('AccountsCtrl', function($scope, Accounts, $localstorage, $state) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
   $scope.switchTo = function(accountId) {
     $localstorage.set('activeId', accountId);
     $localstorage.setObject('activeUser', Accounts.get(accountId));
