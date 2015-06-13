0c18aa2545caec1cee39d6d43ef641a496e4e7ec
Wivorn Chowattanakul <wivorn.chowattanakul@gmail.com>
2015-06-13 18:15:32 +0700

    Changed file names

--- www/js/app.js
+++ www/js/app.js
@@ -38,12 +38,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.personal', {
+    url: '/personal',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-personal': {
+        templateUrl: 'templates/tab-personal.html',
+        controller: 'PersonalCtrl'
       }
     }
   })
@@ -78,6 +78,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/personal');
 
 });
